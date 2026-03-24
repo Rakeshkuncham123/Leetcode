5b211253d9fa4cbb3e6f9dc0ae3fd12b9254a5b8
Rakesh <kunchamrakesh123@gmail.com>
2026-03-24 11:47:39 +0530

    Time: 10 ms (84.31%) | Memory: 91.1 MB (87.74%) - LeetSync

---
+++ 3031-construct-product-matrix/construct-product-matrix.java
@@ -0,0 +1,37 @@
+class Solution {
+    public int[][] constructProductMatrix(int[][] grid) {
+        int n=grid.length;
+        int m=grid[0].length;
+        int mod=12345;
+        int size=n*m;
+        int arr[]=new int[size];
+        int idx=0;
+        for(int i=0;i<n;i++){
+            for(int j=0;j<m;j++){
+                arr[idx]=grid[i][j]%mod;
+                idx++;
+            }
+        }
+        //System.out.println(Arrays.toString(arr));
+
+        int prefix[]=new int[size];
+        prefix[0]=1;
+        for(int i=1;i<size;i++){
+            prefix[i]=(prefix[i-1]*arr[i-1])%mod;
+        }
+        int suffix[]=new int[size];
+        suffix[size-1]=1;
+        for(int i=size-2;i>=0;i--){
+            suffix[i]=(suffix[i+1]*arr[i+1])%mod;
+        }
+        idx=0;
+        int[][] res=new int[n][m];
+        for(int i=0;i<n;i++){
+            for(int j=0;j<m;j++){
+                res[i][j]=(prefix[idx]*suffix[idx])%mod;
+                idx++;
+            }
+        }
+        return res;
+    }
+}
